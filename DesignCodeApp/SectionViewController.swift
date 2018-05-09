5d5d381a4c60c4a395c3b22f25ede5e8c73a6191
Sergio Gomes (1130399) <1130399@isep.ipp.pt>
2018-05-09 22:46:32 +0100

    implemented home screen -> book view , hero view and chapter view

---
+++ DesignCodeApp/SectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SectionViewController.swift
+//  DesignCodeApp
+//
+//  Created by Sergio Gomes on 09/05/18.
+//  Copyright © 2018 Sergio Gomes. All rights reserved.
+//
+
+import UIKit
+
+class SectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
